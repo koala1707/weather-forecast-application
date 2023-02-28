d91638c1088f8311fe8eec99556328b45251e87b
koala1707 <m3tpfbzr@gmail.com>
2023-02-28 10:36:39 +1030

    Added UI Design

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import { TextField, Button, Checkbox } from '@material-ui/core';
 
 function App() {
+
+  const [location, setLocation] = useState('');
+  const [fahrenheit, setFahrenheit] = useState(false);
+
+  const searchNewWeather = (event) => {
+
+  }
+
+  const checkedFahrenheit = (event) => {
+    setFahrenheit(!fahrenheit);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className='app-title'>Weather App</h1>
+      <div className='search-location'>
+      <TextField id='outlined-basic' label='Location' variant='outlined' value={location} onChange={(e) => setLocation(e.target.value)}/>
+      <Button className='submit-button' variant='contained' color='secondary' onClick={searchNewWeather}>Submit</Button>
+      </div>
+      
+      <div className='weather-container'>
+        <div className='fahrenheit'>
+          <label>Fahrenheit</label>
+          <Checkbox checked={fahrenheit} onChange={checkedFahrenheit} label='Fahrenheit'></Checkbox>
+        </div>
+        <p>LOCATION</p>
+      </div>
     </div>
   );
 }
