0a83449a7d400a77743730b1d0d06e0685659773
koala1707 <m3tpfbzr@gmail.com>
2023-03-01 21:41:58 +1030

    Changed API

--- src/App.js
+++ src/App.js
@@ -1,40 +1,44 @@
 import './App.css';
 import React, { useState, useEffect } from 'react';
-import { TextField, Button, Checkbox } from '@material-ui/core';
+import { TextField, Button, Checkbox, TableContainer, TableHead, TableRow, TableBody, TableCell } from '@material-ui/core';
+import { Paper, Table } from '@mui/material'
+import { styled } from '@mui/material/styles';
 import json from './db.json'
 
-var restoreData = [];
-var aveTempList = [];
+
 
 
 // Data which is fetched from API is formated such as [Date, Temperature]
 function collectData(weatherData) {
+  var restoreData = []
   var i = 0
   for(var index in weatherData){
-    let date = weatherData[index]["time"].split("T")[0]
+    let date = weatherData[index]["dt_txt"].split(" ")[0]
+    let temp = weatherData[index]["main"]["temp"]
     if(index == 0){
-      restoreData.push([date,weatherData[index]["airTemperature"]["noaa"]])
+      restoreData.push([date, temp])
     }
     else if(restoreData[i][0] == date){
-      restoreData[i].push(weatherData[index]["airTemperature"]["noaa"])
+      restoreData[i].push(temp)
     }
     else{
-      restoreData.push([date, weatherData[index]["airTemperature"]["noaa"]])
+      restoreData.push([date, temp])
       i += 1
     }
   }
-  return restoreData;
+  var allWeatherData = calculateAverageTemperature(restoreData)
+  return allWeatherData;
 }
 
 // Calculate the average temperature and restore the data into useState.
 function calculateAverageTemperature(data) {
+  var aveTempList = [];
   for(var key in data) {
-    // console.log("res",data[key][1])
     let temparatures = data[key].slice(1)
     let sumTemperature = temparatures.reduce((a, b) => (a + b))
     let averageTemperature = sumTemperature / temparatures.length
     let calculateFahrenheit = (averageTemperature * 1.8) + 32
-    aveTempList.push([restoreData[key][0],averageTemperature.toFixed(2), calculateFahrenheit.toFixed(2)])
+    aveTempList.push([data[key][0],averageTemperature.toFixed(2), calculateFahrenheit.toFixed(2)])
   }
   return aveTempList
 }
@@ -43,23 +47,21 @@ function calculateAverageTemperature(data) {
 function App() {
   const [location, setLocation] = useState('');
   const [fahrenheit, setFahrenheit] = useState(false);
-  const [weatherList, setWeatherList] = useState([])
   const [mapList, setMapList] = useState([])
   const [search, setSearch] = useState(false)
 
   const [latitude, setLatitude] = useState('');
   const [longitude, setLongitude] = useState('');
-  const params = 'waveHeight,airTemperature';
 
-  const GEO_API = 'c2b5525f2a4aac7825c56f17fa158fe0'
+  const API_KEY = 'c2b5525f2a4aac7825c56f17fa158fe0'
 
   const [error, setError] = useState('');
   
-  const searchNewWeather = (event) => {
+  const searchNewWeather = () => {
     setSearch(true)
   }
 
-  const checkedFahrenheit = (event) => {
+  const checkedFahrenheit = () => {
     setFahrenheit(!fahrenheit);
   }
 
@@ -70,61 +72,50 @@ function App() {
   // To get the current position 
   // refer to this website to find the current location: https://www.pluralsight.com/guides/how-to-use-geolocation-call-in-reactjs
   useEffect(() => {
+    console.log("search", search)
+
+    // Search a location
     if(search){
-      fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${GEO_API}`)
+      fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}`)
       .then((res) => res.json()
       .then((geo) => {
+        console.log("geo", geo)
+
         setLatitude(geo.coord.lat)
         setLongitude(geo.coord.lon)
       }))
     }
+    // Current Location
     else{
       navigator.geolocation.getCurrentPosition(function(currentLocation){
         setLatitude(currentLocation.coords.latitude);
         setLongitude(currentLocation.coords.longitude);
       });
-      
     }
     console.log("latitude: ", latitude);
     console.log("longitude: ", longitude);
-      
+  },[latitude, longitude, search]);
+
+  useEffect(() => {
     if(latitude && longitude != null){
-      fetch(`https://api.stormglass.io/v2/weather/point?lat=${latitude}&lng=${longitude}&params=${params}`, {
-      headers: {
-        'Authorization': '5c1f56e4-b685-11ed-bce5-0242ac130002-5c1f5856-b685-11ed-bce5-0242ac130002'
-      }
-    })
-    .then(response => {
-      if(!response.ok){
-        if(response.status == 402){
-          throw Error('API runs over 5 times. Need to pay.')
+      fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`)
+      .then(response => {
+        if(!response.ok){
+          throw Error(`Cannot fetch data from API. Error(${response.status})`)
         }
-        else{
-          throw Error(`Error ${response.status}. Couldn't fetch data.`)
-        }
-      }
-      return response.json()
-    })
-    .then(weatherResult => {
-      setWeatherList(weatherResult["hours"])
-      let collectedData = collectData(weatherList)
-      // Problem: First a couple of days are broken down into 24 hours not just a day.
-      // Solution: Restore data to calculate the average temparature. 
-      let allData = calculateAverageTemperature(collectedData)
-      allData.map((data, index) => {
-        setMapList(mapList => mapList.concat({date: data[0], cdegree: data[1], fdegree: data[2]}));
+        return response.json()
       })
-    })
-    .catch (error => {
-      setError(error.message)
-      // console.log(error.message);
-    })
-    console.log("weather", weatherList)
-    console.log("map",mapList)
+      .then(weatherResult => {
+        let collectedData = collectData(weatherResult['list'])
+        collectedData.map((data, index) => {
+          setMapList(mapList => mapList.concat({date: data[0], cdegree: data[1], fdegree: data[2]}))
+        })
+      })
+      .catch (error => {
+        setError(error.message)
+      })
     }
-    
-    
-  },[latitude, longitude, search])
+  },[latitude, longitude])
 
   return (
     <div>
@@ -140,21 +131,34 @@ function App() {
           <Checkbox checked={fahrenheit} onChange={checkedFahrenheit} label='Fahrenheit'></Checkbox>
         </div>
         <h2>Weather Forecast</h2>
+        <h3 className='location'>{search ? 'Searched Location' : 'Current Location'}</h3>
         <div>
           {error && <div className='payment-error'>{ error }</div>}
-        {mapList.slice(0,7).map((d, i) => {
-          return(
-          <div key={i}>
-            <li>Date</li>
-            <p>{d.date}</p>
-            <li>Temperature</li>
-            <p>{fahrenheit ? `${d.fdegree}F` : `${d.cdegree}C` }</p>
-          </div>
-          )})}
+              <TableContainer component={Paper}>
+                <Table sx={{ minWidth: 500}} aria-lable="test">
+                  <TableHead>
+                    <TableRow>
+                      <TableCell>Date</TableCell>
+                      <TableCell>Temperature</TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    {mapList.map((d, i) => {
+                      return(
+                        <TableRow key={i}>
+                          <TableCell component="th" scope="row">{d.date}</TableCell>
+                          <TableCell>{fahrenheit ? `${d.fdegree}F` : `${d.cdegree}C`}</TableCell>
+                        </TableRow>          
+                      )
+                    })}
+                  </TableBody>
+                </Table>
+              </TableContainer>
         </div>
       </div>
     </div>
   );
 }
 
+export { collectData, calculateAverageTemperature }
 export default App;
